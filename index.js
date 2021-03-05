1e05e092cbcf4fc68c38e58f26b767a01968b353
Blake Runyon <blakerunyon09@gmail.com>
2021-03-05 16:01:36 -0700

    Added First Fetch

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Cha.")
+const asanaBackendURL = "https://asana-backend.herokuapp.com"
+const usersIndex = "/users/"
+const body = document.querySelector('body')
+
+fetch(asanaBackendURL + usersIndex)
+  .then(response => response.json())
+  .then(users => users.forEach(
+    user => {
+    const h1 = document.createElement('h1')
+
+    h1.textContent = user.username
+
+    body.append(h1)
+  }))
