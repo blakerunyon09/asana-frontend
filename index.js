1e7e19ee11556da87d6c90ee6d061d442ddafb1c
Blake Runyon <blakerunyon09@gmail.com>
2021-03-05 17:10:43 -0700

    Added Some Body Content

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const asanaBackendURL = "https://asana-backend.herokuapp.com"
 const usersIndex = "/users/"
+const cardsIndex = "/cards/"
 const body = document.querySelector('body')
+const cardList = document.querySelector('#cards')
 
 fetch(asanaBackendURL + usersIndex)
   .then(response => response.json())
@@ -12,3 +14,16 @@ fetch(asanaBackendURL + usersIndex)
 
     body.append(h1)
   }))
+
+fetch(asanaBackendURL + cardsIndex)
+  .then(response => response.json())
+  .then(cards => cards.forEach(
+    card => {
+      const p = document.createElement('p')
+      const p2 = document.createElement('p')
+
+      p.textContent = card.name
+      p2.textContent = card.description
+
+      cardList.append(p, p2)
+    }))
