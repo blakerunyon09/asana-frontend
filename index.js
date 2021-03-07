517866bae6823d9e0b6bbdd9271d7348e51bb162
Blake Runyon <blakerunyon09@gmail.com>
2021-03-06 18:43:26 -0700

    Created Nav & Cards

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 const asanaBackendURL = "https://asana-backend.herokuapp.com"
 const usersIndex = "/users/"
 const cardsIndex = "/cards/"
-const body = document.querySelector('body')
-const cardList = document.querySelector('#cards')
-
-fetch(asanaBackendURL + usersIndex)
-  .then(response => response.json())
-  .then(users => users.forEach(
-    user => {
-    const h1 = document.createElement('h1')
-
-    h1.textContent = user.username
-
-    body.append(h1)
-  }))
+const cardContainer = document.querySelector('#card_container')
 
 fetch(asanaBackendURL + cardsIndex)
   .then(response => response.json())
   .then(cards => cards.forEach(
     card => {
+      const div = document.createElement('div')
+      div.classList.add('card')
+      
+      const h3 = document.createElement('h3')
+      h3.textContent = card.name
+      div.classList.add('font-bold')
+      div.append(h3)
+
       const p = document.createElement('p')
-      const p2 = document.createElement('p')
+      p.textContent = card.description
+      p.classList.add('text-sm','pt-1', 'font-normal')
+      div.append(p)
 
-      p.textContent = card.name
-      p2.textContent = card.description
-
-      cardList.append(p, p2)
+      cardContainer.append(div)
     }))
